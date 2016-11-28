5e0755918095b723da5b46b0855c0b1666a72a94
hellsurfer <emanuel_knecht@hotmail.com>
2016-11-28 19:27:23 +0100

    initial commit

---
+++ MuckTests/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuckTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
